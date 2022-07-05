4300dd2a6947765a6c9ed8246cfde96e6e67e2a4
nahee101 <bagsj26@gmail.com>
2022-07-05 14:25:41 +0900

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>프로그레시브 웹앱</h1>
+        <h3>설치/이동 아이콘을 이용해서 앱 화면을 띄울 수 있다.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
